6ef2c39c2e174f903ae2bfd03d876a9bb49ddcc6
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2022-03-26 19:11:50 -0300

    using typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,9 +1,16 @@
-import { RepositoryItem } from "./RepositoryItem"
-import '../styles/repositories.scss'
 import { useState, useEffect } from "react"
+import { RepositoryItem } from "./RepositoryItem"
+
+import '../styles/repositories.scss'
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -16,7 +23,7 @@ export function RepositoryList() {
   return <section className="repository-list">
     <h1>Lista de repositórios</h1>
     <ul>
-      {repositories.map(repository => <RepositoryItem key={repository.id} repository={repository} />)}
+      {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />)}
     </ul>
   </section>
 }
